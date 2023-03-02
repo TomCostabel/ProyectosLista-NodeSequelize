fe95f4b05a93794259c2fd84989f306223388df8
TomCostabel <tomasperalta1997@hotmail.com>
2023-03-02 11:00:28 -0300

    deploy backend

--- src/database/database.js
+++ src/database/database.js
@@ -6,7 +6,7 @@ const { DB_PASSWORD, DB_USER, DB_HOST, DB_NAME } = process.env;
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
-    dialect: "postgres",
+    dialect: DB_USER,
     //-----> Aca deshabilito los mensajes por consola !
     logging: false,
 });
